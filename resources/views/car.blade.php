b5189fd7f0555acf28a853e7762062359175ed08
VivifyIdeas <vivify@VivifyIdeass-MacBook-Pro-4.local>
2022-05-20 21:37:49 +0200

    HTMLe promene na cars i car stranicama

--- resources/views/car.blade.php
+++ resources/views/car.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 @section('title', 'Single car')
 @section('content')
+    <h1>Information about a car:</h1>
         <ul>
             <li>ID: {{$car->id}}</li>
             <li>Model: {{$car->title}}</li>
--- resources/views/cars.blade.php
+++ resources/views/cars.blade.php
@@ -1,10 +1,11 @@
 @extends('layouts.app')
 @section('title', 'All cars')
 @section('content')
-        <ul>
-            @foreach($cars as $car)
-            <li><a href="/car/{{$car->id}}">Model: {{$car->title}}</a></li>
+<h1>List of cars:</h1>
+    <ul>
+        @foreach($cars as $car)
+        <li><a href="/car/{{$car->id}}">Model: {{$car->title}}</a></li>
 
-            @endforeach
-        </ul>
+         @endforeach
+    </ul>
 @endsection
