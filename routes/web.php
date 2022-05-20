a07ea9b87b44313596e0c3d93f0fb00e8ed7ae2e
VivifyIdeas <vivify@VivifyIdeass-MacBook-Pro-4.local>
2022-05-20 18:55:50 +0200

    Dodate dinamicke promenljive za ime i godine

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,9 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $name = 'Ksenija';
+    $age = 23;
+    return view('welcome', compact('name', 'age'));
 });
 
 Route::get('/about-us', function () {
