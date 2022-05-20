f8a25edbf8e72e1978b93feb2e8dc1af6cdc69bd
VivifyIdeas <vivify@VivifyIdeass-MacBook-Pro-4.local>
2022-05-20 20:13:42 +0200

    Izlistavanje nazive automobila preko controllera i modela

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CarController;
 
 /*
 |--------------------------------------------------------------------------
@@ -22,3 +23,5 @@ Route::get('/', function () {
 Route::get('/about-us', function () {
     return 'About us';
 });
+
+Route::get('/cars', [CarController::class, 'index']);
